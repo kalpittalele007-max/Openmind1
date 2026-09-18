c89a013801eb752111c843a2423dd89376f413d4
Kalpit Talele <kalpit.talele007@gmail.com>
2026-09-18 18:36:17 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -7,32 +7,35 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
     private WebView webView;
 
-    // For the first local-phone setup. Change to your HTTPS server later if needed.
-    private static final String OPENMIND_URL = "http://127.0.0.1:3000";
+    private static final String OPENMIND_URL =
+            "http://127.0.0.1:3000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         webView = new WebView(this);
+
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
-        settings.setAllowFileAccess(false);
-        settings.setAllowContentAccess(true);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.setBackgroundColor(0xFF080808);
         webView.loadUrl(OPENMIND_URL);
+
         setContentView(webView);
     }
 
     @Override
     public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
